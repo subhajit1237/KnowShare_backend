9df103fd341792766b47f5c076304108d641ced1
Subhajit Das <babai.dassubhajit@gmail.com>
2023-03-05 19:12:47 +0530

    controllers and signup setup

---
+++ app/middleware/verifySignUp.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+const ROLES = db.ROLES;
+const User = db.user;
+
+checkDuplicateUsernameOrEmail = (req, res, next) => {
+    // username
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(user => {
+        if(user) {
+            res.status(400).send({
+                message: "Failed! Username is already in use!"
+            });
+            return;
+        }
+
+        // email
+        User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(user => {
+            if(user) {
+                res.status(400).send({
+                    message: "Failed! Email is already in use!"
+                });
+                return;
+            }
+
+            next();
+        });
+    });
+};
+
--- server.js
+++ server.js
@@ -16,6 +16,23 @@ app.get("/", (req,res) => {
     res.json({ message: "Welcome to KnowShare."});
 });
 
+const db = require("./app/models");
+db.sequelize.sync();
+// const postgres = require('pg');
+// require('dotenv').config();
+
+// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+// const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+// const sql = postgres(URL, { ssl: 'require' });
+
+// async function getPgVersion() {
+//   const result = await sql`select version()`;
+//   console.log(result);
+// }
+
+// getPgVersion();
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log((`Server is running on port ${port}`));
