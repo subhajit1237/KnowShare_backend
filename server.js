5721002ebedd1560c1b811e9f86fb758e8edd141
Subhajit Das <babai.dassubhajit@gmail.com>
2023-03-08 11:13:56 +0530

    auth done 99%

--- server.js
+++ server.js
@@ -34,6 +34,36 @@ app.get("/", (req,res) => {
 
 // getPgVersion();
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log((`Server is running on port ${port}`));
