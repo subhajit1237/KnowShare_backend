0290a02130ccd8954c6384b69784575c0931621a
Subhajit Das <babai.dassubhajit@gmail.com>
2023-03-12 14:07:35 +0530

    db updated

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "123",
-    DB: "testdb",
+    HOST: process.PGHOST,
+    USER: process.PGUSER,
+    PASSWORD: process.PGPASSWORD,
+    DB: process.PGDATABASE,
+    ENDPOINT: process.ENDPOINT_ID,
     dialect: "postgres",
     pool: {
         max: 10,
