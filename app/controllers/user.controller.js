421b7147300f0298e89b24bbc4db9eaed7da4a42
Subhajit Das <babai.dassubhajit@gmail.com>
2023-03-16 13:35:57 +0530

    controllers updated

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,15 +1,16 @@
-exports.allAccess = (req,res) => {
+exports.allAccess = (req, res) => {
     res.status(200).send("Public Content.");
-};
-
-exports.userBoard = (req,res) => {
+  };
+  
+  exports.userBoard = (req, res) => {
     res.status(200).send("User Content.");
-};
-
-exports.adminBoard = (req,res) => {
+  };
+  
+  exports.adminBoard = (req, res) => {
     res.status(200).send("Admin Content.");
-};
-
-exports.moderatorBoard = (req,res) => {
-    res.status(200).send("Moderator Content");
-};
+  };
+  
+  exports.moderatorBoard = (req, res) => {
+    res.status(200).send("Moderator Content.");
+  };
+  
